0855c23ac5a14a5753aebaf2aa78af36724d310a
Francesco Novy <francesco@fabscale.com>
2021-12-07 17:13:00 +0100

    fix: Ensure we remove the prerender config from the empty file

--- packages/ember-build-prerender/lib/utils/merge-prerender.js
+++ packages/ember-build-prerender/lib/utils/merge-prerender.js
@@ -8,14 +8,14 @@ module.exports = async function mergePrerender({
 }) {
   await fs.copy(buildDir, outputPath);
 
-  // Rename 404.index to emptyFile
+  // Rename 404.html to emptyFile
   let emptyFilePath = path.join(outputPath, emptyFile);
   await fs.rename(path.join(outputPath, '404.html'), emptyFilePath);
 
   // Remove prerender config from empty file
   let emptyFileContent = await fs.readFile(emptyFilePath, 'utf-8');
   emptyFileContent = emptyFileContent.replace(
-    '<meta name="prerender-config" stage="should-prerender"></meta>',
+    '<meta name="prerender-config" content="should-prerender"></meta>',
     ''
   );
   await fs.writeFile(emptyFilePath, emptyFileContent, 'utf-8');
